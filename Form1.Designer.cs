f6cc12ebe9bcfd496892dc847d52be65a662c841
MarianaP <emarianap115@gmail.com>
2021-02-27 15:34:40 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿
+namespace Ejercicio2
+{
+    partial class registroDePersonas
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guaradarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cbPais = new System.Windows.Forms.ComboBox();
+            this.cbEstado = new System.Windows.Forms.ComboBox();
+            this.chkAcepto = new System.Windows.Forms.CheckBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.tbApellidoP = new System.Windows.Forms.TextBox();
+            this.tbApellidoM = new System.Windows.Forms.TextBox();
+            this.rbMasc = new System.Windows.Forms.RadioButton();
+            this.rbFem = new System.Windows.Forms.RadioButton();
+            this.rbResp = new System.Windows.Forms.RadioButton();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblApellidoP = new System.Windows.Forms.Label();
+            this.lblApellidoM = new System.Windows.Forms.Label();
+            this.lblPais = new System.Windows.Forms.Label();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(534, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevoToolStripMenuItem,
+            this.abrirToolStripMenuItem,
+            this.guaradarToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            this.nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            this.nuevoToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.nuevoToolStripMenuItem.Text = "Nuevo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            // 
+            // guaradarToolStripMenuItem
+            // 
+            this.guaradarToolStripMenuItem.Name = "guaradarToolStripMenuItem";
+            this.guaradarToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.guaradarToolStripMenuItem.Text = "Guardar";
+            this.guaradarToolStripMenuItem.Click += new System.EventHandler(this.guaradarToolStripMenuItem_Click);
+            // 
+            // cbPais
+            // 
+            this.cbPais.FormattingEnabled = true;
+            this.cbPais.Items.AddRange(new object[] {
+            "México",
+            "Estados Unidos"});
+            this.cbPais.Location = new System.Drawing.Point(104, 129);
+            this.cbPais.Name = "cbPais";
+            this.cbPais.Size = new System.Drawing.Size(121, 21);
+            this.cbPais.TabIndex = 1;
+            // 
+            // cbEstado
+            // 
+            this.cbEstado.FormattingEnabled = true;
+            this.cbEstado.Items.AddRange(new object[] {
+            "San Luis Potosí",
+            "CDMX"});
+            this.cbEstado.Location = new System.Drawing.Point(104, 156);
+            this.cbEstado.Name = "cbEstado";
+            this.cbEstado.Size = new System.Drawing.Size(121, 21);
+            this.cbEstado.TabIndex = 2;
+            // 
+            // chkAcepto
+            // 
+            this.chkAcepto.AutoSize = true;
+            this.chkAcepto.Location = new System.Drawing.Point(288, 246);
+            this.chkAcepto.Name = "chkAcepto";
+            this.chkAcepto.Size = new System.Drawing.Size(60, 17);
+            this.chkAcepto.TabIndex = 3;
+            this.chkAcepto.Text = "Acepto";
+            this.chkAcepto.UseVisualStyleBackColor = true;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(252, 33);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 4;
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(104, 33);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(100, 20);
+            this.tbNombre.TabIndex = 5;
+            this.tbNombre.TextChanged += new System.EventHandler(this.tbNombre_TextChanged);
+            // 
+            // tbApellidoP
+            // 
+            this.tbApellidoP.Location = new System.Drawing.Point(104, 63);
+            this.tbApellidoP.Name = "tbApellidoP";
+            this.tbApellidoP.Size = new System.Drawing.Size(100, 20);
+            this.tbApellidoP.TabIndex = 6;
+            // 
+            // tbApellidoM
+            // 
+            this.tbApellidoM.Location = new System.Drawing.Point(104, 89);
+            this.tbApellidoM.Name = "tbApellidoM";
+            this.tbApellidoM.Size = new System.Drawing.Size(100, 20);
+            this.tbApellidoM.TabIndex = 7;
+            // 
+            // rbMasc
+            // 
+            this.rbMasc.AutoSize = true;
+            this.rbMasc.Location = new System.Drawing.Point(104, 200);
+            this.rbMasc.Name = "rbMasc";
+            this.rbMasc.Size = new System.Drawing.Size(73, 17);
+            this.rbMasc.TabIndex = 8;
+            this.rbMasc.TabStop = true;
+            this.rbMasc.Text = "Masculino";
+            this.rbMasc.UseVisualStyleBackColor = true;
+            // 
+            // rbFem
+            // 
+            this.rbFem.AutoSize = true;
+            this.rbFem.Location = new System.Drawing.Point(104, 223);
+            this.rbFem.Name = "rbFem";
+            this.rbFem.Size = new System.Drawing.Size(71, 17);
+            this.rbFem.TabIndex = 9;
+            this.rbFem.TabStop = true;
+            this.rbFem.Text = "Femenino";
+            this.rbFem.UseVisualStyleBackColor = true;
+            // 
+            // rbResp
+            // 
+            this.rbResp.AutoSize = true;
+            this.rbResp.Location = new System.Drawing.Point(104, 246);
+            this.rbResp.Name = "rbResp";
+            this.rbResp.Size = new System.Drawing.Size(126, 17);
+            this.rbResp.TabIndex = 10;
+            this.rbResp.TabStop = true;
+            this.rbResp.Text = "Prefiero no responder";
+            this.rbResp.UseVisualStyleBackColor = true;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(42, 40);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 11;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblApellidoP
+            // 
+            this.lblApellidoP.AutoSize = true;
+            this.lblApellidoP.Location = new System.Drawing.Point(14, 66);
+            this.lblApellidoP.Name = "lblApellidoP";
+            this.lblApellidoP.Size = new System.Drawing.Size(84, 13);
+            this.lblApellidoP.TabIndex = 12;
+            this.lblApellidoP.Text = "Apellido Paterno";
+            // 
+            // lblApellidoM
+            // 
+            this.lblApellidoM.AutoSize = true;
+            this.lblApellidoM.Location = new System.Drawing.Point(14, 96);
+            this.lblApellidoM.Name = "lblApellidoM";
+            this.lblApellidoM.Size = new System.Drawing.Size(86, 13);
+            this.lblApellidoM.TabIndex = 13;
+            this.lblApellidoM.Text = "Apellido Materno";
+            // 
+            // lblPais
+            // 
+            this.lblPais.AutoSize = true;
+            this.lblPais.Location = new System.Drawing.Point(42, 132);
+            this.lblPais.Name = "lblPais";
+            this.lblPais.Size = new System.Drawing.Size(29, 13);
+            this.lblPais.TabIndex = 14;
+            this.lblPais.Text = "País";
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Location = new System.Drawing.Point(42, 164);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(40, 13);
+            this.lblEstado.TabIndex = 15;
+            this.lblEstado.Text = "Estado";
+            // 
+            // registroDePersonas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 450);
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.lblPais);
+            this.Controls.Add(this.lblApellidoM);
+            this.Controls.Add(this.lblApellidoP);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.rbResp);
+            this.Controls.Add(this.rbFem);
+            this.Controls.Add(this.rbMasc);
+            this.Controls.Add(this.tbApellidoM);
+            this.Controls.Add(this.tbApellidoP);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.chkAcepto);
+            this.Controls.Add(this.cbEstado);
+            this.Controls.Add(this.cbPais);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "registroDePersonas";
+            this.Text = "Registro de Personas";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuevoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guaradarToolStripMenuItem;
+        private System.Windows.Forms.ComboBox cbPais;
+        private System.Windows.Forms.ComboBox cbEstado;
+        private System.Windows.Forms.CheckBox chkAcepto;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.TextBox tbApellidoP;
+        private System.Windows.Forms.TextBox tbApellidoM;
+        private System.Windows.Forms.RadioButton rbMasc;
+        private System.Windows.Forms.RadioButton rbFem;
+        private System.Windows.Forms.RadioButton rbResp;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblApellidoP;
+        private System.Windows.Forms.Label lblApellidoM;
+        private System.Windows.Forms.Label lblPais;
+        private System.Windows.Forms.Label lblEstado;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Ejercicio2
+{
+    public partial class registroDePersonas : Form
+    {
+        List<string> info;
+        public registroDePersonas()
+        {
+            info = new List<string>(8);
+          
+            InitializeComponent();
+        }
+
+        private void tbNombre_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void guaradarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string datos = "";
+            info.Add(tbNombre.Text);
+            info.Add(tbApellidoP.Text);
+            info.Add(tbApellidoM.Text);
+            info.Add(cbPais.SelectedItem.ToString());
+            info.Add(cbEstado.SelectedItem.ToString());
+            if (rbMasc.Checked)
+                info.Add(rbMasc.Text);
+            else
+                if (rbFem.Checked)
+                info.Add(rbFem.Text);
+            else
+                info.Add(rbResp.Text);
+            info.Add(chkAcepto.Enabled.ToString());
+            info.Add(monthCalendar1.SelectionRange.Start.ToString());
+            foreach (string elemento in info)
+                datos += elemento;
+            File.WriteAllText(@"C:\Users\DELL\Desktop\UNI MARIANA\INTERFACES\\info.txt", datos);
+
+        }
+
+    }
+}
